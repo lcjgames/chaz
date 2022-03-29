7dd11c81954b6cd768ecd27990fedb92c3363491
Luiz Chagas Jardim <luiz.jardim@velocix.com>
2022-03-29 20:32:06 +0100

    GET

--- src/score.rs
+++ src/score.rs
@@ -1,3 +1,5 @@
+use wasm_bindgen_futures::spawn_local;
+
 use crate::options::Difficulty;
 
 pub struct Score {
@@ -7,8 +9,22 @@ pub struct Score {
     pub time: f32,
 }
 
+pub async fn fetch() -> Result<String, reqwest::Error> {
+    let res = reqwest::Client::new()
+        .get("http://127.0.0.1:8001/highscores/list")
+        .header("Accept", "application/vnd.github.v3+json")
+        .header("Access-Control-Allow-Origin", "Any")
+        .send()
+        .await?;
+
+    Ok(res.text().await?)
+}
+
 pub fn get_scores(level: usize, difficulty: Difficulty) -> Vec<Score> {
-    //TODO: fetch from backend
+    spawn_local(async {
+        let scores = fetch().await;
+        crate::log::console_log!("{:?}", scores);
+    });
     vec![
         Score {
             name: "luiz".to_string(),
